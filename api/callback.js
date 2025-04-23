3c60834d08e84ed90956cbc1cf0326dbd8adad83
Abdul Kader <abdulkader0126@gmail.com>
2025-04-23 16:33:55 +0700

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -2,7 +2,11 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
     console.log('🔥 Headers:', req.headers);
     console.log('📦 Body:', req.body);
-    res.status(200).json({ message: 'Callback diterima' });
+
+    res.status(200).json({
+      status: 'success',
+      received: req.body
+    });
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
   }
