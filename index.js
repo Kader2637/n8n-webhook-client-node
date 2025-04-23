2419b6f741d8cc43101e1c3a6bfb23812c8fac91
Abdul Kader <abdulkader0126@gmail.com>
2025-04-23 16:31:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-
-app.post('/callback', (req, res) => {
-    console.log('Panggilan callback diterima:', req.body);
-    res.status(200).send('Callback diterima');
-  });
-  
-
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
+Silahkan gunakan https://n8n-webhook-client-node.vercel.app/api/callback untuk mendapatkan callback
